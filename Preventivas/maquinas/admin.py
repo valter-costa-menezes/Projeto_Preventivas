02f2f021af52e0522a76c1db7e2006f8f34afad9
valter-costa-menezes <151465415+valter-costa-menezes@users.noreply.github.com>
2024-08-03 14:24:12 -0300

    Delete Preventivas directory
    vou comentar todo o codigo

--- Preventivas/maquinas/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from maquinas.models import Patrimonio
-from maquinas.models import UltimaPreventiva
-
-admin.site.register(Patrimonio)
-admin.site.register(UltimaPreventiva)
--- Preventivas/maquinas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MaquinasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'maquinas'
